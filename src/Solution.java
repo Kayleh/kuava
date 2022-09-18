5941ff45cb153ef8d932f4e89fb389091d2310df
w1z4Rd <yaojinqing@kayleh.top>
2022-09-18 23:47:01 +0800

    Trie树

--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,60 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Solution {
-    public List<Long> maximumEvenSplit(long finalSum) {
-        List<Long> ans = new ArrayList<>();
-        if ((finalSum & 1) == 0) {
-            long k = 2;
-            while (true) {
-                if (finalSum == k) {
-                    ans.add(k);
-                    break;
-                } else if (finalSum < k) {
-                    ans.add(ans.remove(ans.size() - 1) + finalSum);
-                    break;
-                } else {
-                    ans.add(k);
-                    finalSum -= k;
-                    k += 2;
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            if (level % 2 == 1) {
+                //反转该层的每一个节点的值
+                List<TreeNode> nodes = new ArrayList<>(queue);
+                for (int i = 0; i < nodes.size() / 2; i++) {
+                    TreeNode treeNode = nodes.get(i);
+                    TreeNode treeNode1 = nodes.get(nodes.size() - 1 - i);
+                    int temp = treeNode.val;
+                    treeNode.val = treeNode1.val;
+                    treeNode1.val = temp;
                 }
             }
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            level++;
         }
-        return ans;
+        return root;
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
