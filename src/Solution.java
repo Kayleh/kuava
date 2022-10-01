d2b05aa945999d542bd1f182215a087bb50d051c
w1z4Rd <yaojinqing@kayleh.top>
2022-10-01 21:37:40 +0800

    auto update~

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    public int longestSubarray(int[] nums) {
-        //按位与（bitwise AND）运算得到的值 最大 的 非空 子数组。
-        //令 k 是 nums 任意 子数组执行按位与运算所能得到的最大值。那么，只需要考虑那些执行一次按位与运算后等于 k 的子数组。
-
-        // 1. 求出 nums 中的最大值 max
-        int max = 0;
-        for (int num : nums) {
-            max = Math.max(max, num);
-        }
-        // 2. 求出 max 的二进制表示中 1 的个数
-        int count = 0;
-        while (max > 0) {
-            max >>= 1;
-            count++;
-        }
-        // 3. 求出 2^count - 1
-        int k = (1 << count) - 1;
-        // 4. 求出 nums 中执行一次按位与运算后等于 k 的子数组的最大长度
-        int res = 0;
-        int left = 0, right = 0;
-        int sum = 0;
-        while (right < nums.length) {
-            sum += nums[right];
-            while (sum > k) {
-                sum -= nums[left];
-                left++;
+    public int[][] highestPeak(int[][] isWater) {
+        int n = isWater.length;
+        int m = isWater[0].length;
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[i][j] = -1;
             }
-            res = Math.max(res, right - left + 1);
-            right++;
         }
-
-
-        return res - 1;
+        // bfs
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (isWater[i][j] == 1) {
+                    ans[i][j] = 0; // 从水开始 bfs
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int x = cur[0] + dir[i][0];
+                int y = cur[1] + dir[i][1];
+                if (x >= 0 && x < n && y >= 0 && y < m && ans[x][y] == -1) {
+                    ans[x][y] = ans[cur[0]][cur[1]] + 1;
+                    q.add(new int[]{x, y});
+                }
+            }
+        }
+        return ans;
     }
 }
