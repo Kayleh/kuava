c7ceef90d216f148b9dfbf1e892bb04100810c1c
w1z4Rd <yaojinqing@kayleh.top>
2022-10-02 19:30:53 +0800

    auto update~

--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,26 @@
 import java.util.LinkedList;
-import java.util.Queue;
 
 class Solution {
-    public int[][] highestPeak(int[][] isWater) {
-        int n = isWater.length;
-        int m = isWater[0].length;
-        int[][] ans = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                ans[i][j] = -1;
-            }
-        }
-        // bfs
-        Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (isWater[i][j] == 1) {
-                    ans[i][j] = 0; // 从水开始 bfs
-                    q.add(new int[]{i, j});
+    public int compress(char[] chars) {
+        LinkedList<Character> res = new LinkedList<>();
+        int cnt = 1;
+        char prev = chars[0];
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == prev) {
+                cnt++;
+            } else {
+                res.add(prev);
+                if (cnt > 1) {
+                    for (char c : String.valueOf(cnt).toCharArray()) {
+                        res.add(c);
+                    }
                 }
+                cnt = 1;
+                prev = chars[i];
             }
         }
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int x = cur[0] + dir[i][0];
-                int y = cur[1] + dir[i][1];
-                if (x >= 0 && x < n && y >= 0 && y < m && ans[x][y] == -1) {
-                    ans[x][y] = ans[cur[0]][cur[1]] + 1;
-                    q.add(new int[]{x, y});
-                }
-            }
-        }
-        return ans;
+
+        return res.size();
     }
 }
