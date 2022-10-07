49c06fae4d1d42ab4b8e4aaba44bb2db49e60d8e
w1z4Rd <yaojinqing@kayleh.top>
2022-10-08 01:51:10 +0800

    auto update~

--- src/Solution.java
+++ src/Solution.java
@@ -1,61 +1,27 @@
-import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.io.*;
+import java.util.*;
 
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int Leetcode(String[] words) {
-        String target = "helloleetcode";
+    int[] dx = {0, 1, 0, -1},dy = {1, 0, -1, 0},dx8 = {0, 1, 1, 1, 0, -1, -1, -1},dy8 = {1, 1, 0, -1, -1, -1, 0, 1};
+    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}},dir8 = {{0, 1}, {1, 0}, {0, -1}, {-1, 0},{1,1},{1,-1},{-1,1},{-1,-1}};
+    int INF = 0x3f3f3f3f,MOD = 1000000007, mod = 998244353,maxn = (int)1e5+5,N = 100001;
+    <T> List<T> distince(List<T> list, Function<? super T, ?> f) {Set<Object> set = new HashSet<>();return list.stream().filter(t -> set.add(f.apply(t))).collect(Collectors.toList());}
+    Comparator<int[]> cmp = (a,b)->{if(a[1] == b[1]) return a[0]-b[0];return a[1]-b[1];};
+    boolean check(int x,int[] arr){return x>=0 && x<arr.length;}
+    boolean check(int x,int y,int[][] arr){return x>=0 && x<arr.length && y>=0 && y<arr[0].length;}
+    <T> int binarySearch(List<T> list, T target, Comparator<? super T> c) {int l = 0, r = list.size() - 1;while (l <= r) {int mid = l + (r - l) / 2;if (c.compare(list.get(mid), target) == 0) {return mid;} else if (c.compare(list.get(mid), target) < 0) {l = mid + 1;} else {r = mid - 1;}}return -1;}
+    <T> void prints(Collection<T> list){list.forEach(t->out.print(t.toString()+" "));out.println();}
+    <T> boolean nextPermutation(List<T> list, Comparator<? super T> c) {int n = list.size();int i = n - 2;while (i >= 0 && c.compare(list.get(i), list.get(i + 1)) >= 0) {i--;}if (i < 0) {return false;}int j = n - 1;while (j >= 0 && c.compare(list.get(i), list.get(j)) >= 0) {j--;}Collections.swap(list, i, j);Collections.reverse(list.subList(i + 1, n));return true;}
+    <T> List<T> deepCopy(List<T> src) {ByteArrayOutputStream byteOut = new ByteArrayOutputStream();try {ObjectOutputStream out = new ObjectOutputStream(byteOut);out.writeObject(src);} catch (IOException e) {e.printStackTrace();}ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());try {ObjectInputStream in = new ObjectInputStream(byteIn);return (List<T>) in.readObject();} catch (IOException | ClassNotFoundException e) {e.printStackTrace();}return null;}
+    Optional<Integer> poss;
 
-        //
-        // 可以从一个单词中多次取字母，每个字母仅可被取一次
-        // 从多个单词中取字母，使得取得的字符串为 helloleetcode
-        // 返回取得字符串 helloleetcode 的最小代价
-
-        //动态规划
-        // dp[i][j]表示从第i个单词中取字母，取得字符串为need.substring(0,j)的最小代价
-        // dp[i][j] = min(dp[i-1][j], dp[i-1][j-1] + cost[i][j])
-
-        int n = words.length;
-        int m = target.length();
-        int[][] dp = new int[n + 1][m + 1];
-
-        //初始化
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-
-        //计算dp
-        //从一个单词中取一个字母所需要的代价，为该字母左边和右边字母数量之积, 取完后，该字母不再可用
-
-
-
-        //从一个单词中取一个字母所需要的代价，为该字母左边和右边字母数量之积
-        System.out.println(Arrays.deepToString(dp));
-
-        return dp[n - 1][m - 1];
-
-    }
-
-    private int cost(String word, String substring) {
-
-
-        if (word.contains(substring)) {
-            int index = word.indexOf(substring);
-            int left = index;
-            int right = word.length() - index - 1;
-            return left * right;
-        } else {
-            return Integer.MAX_VALUE;
-        }
-    }
-
-
-    int getCost(String s, int index) {
-        return index * (s.length() - index - 1);
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        //words = ["hold","engineer","cost","level"]
-        String[] words = {"hold", "engineer", "cost", "level"};
-        System.out.println(solution.Leetcode(words));
-    }
 }
+//leetcode submit region end(Prohibit modification and deletion)
