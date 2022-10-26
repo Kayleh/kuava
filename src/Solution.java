abdb9c1173422ed61034d741ba2eab201abfd873
w1z4Rd <yaojinqing@kayleh.top>
2022-10-27 00:34:43 +0800

    auto update~

--- src/Solution.java
+++ src/Solution.java
@@ -1,59 +1,11 @@
+import java.io.Serializable;
+
 class Solution {
-    public int subarrayGCD(int[] nums, int k) {
-        //统计并返回 nums 的子数组中元素的最大公因数等于 k 的子数组数目。
-
-        //数组的最大公因数 是能整除数组中所有元素的最大整数。
-        // 例如，数组 [4,6,16] 的最大公因数是 2 。
-
-
-        int n = nums.length;
-
-
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = gcd(dp[i - 1], nums[i]);
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (dp[i] % k == 0) {
-                ans++;
-            }
-        }
-
-        
-        return ans;
-    }
-
-    /**
-     * 求最大公因数
-     *
-     * @param num
-     * @param k
-     * @return
-     */
-    private int gcd(int num, int k) {
-        if (num == 0) return k;
-        return gcd(k % num, num);
-    }
-
     public static void main(String[] args) {
-        int[] nums = {9, 3, 1, 2, 6, 3};
-        int k = 3;
-        Solution solution = new Solution();
-        int ans = solution.subarrayGCD(nums, k);
-        System.out.println(ans);
+        int count = 0;// 统计1到100000中19的倍数
+        // 使用截尾法
 
-        nums = new int[]{5};
-        k = 1;
-        ans = solution.subarrayGCD(nums, k);
-        System.out.println(ans);
-
-
-        //数组的最大公因数 是能整除数组中所有元素的最大整数。
-        nums = new int[]{7, 5, 6, 8, 3};
-        k = 5;
-
+        System.out.println(count);
     }
+
 }
--- src/template/cpp/Solution.cpp
+++ src/template/cpp/Solution.cpp
@@ -6,17 +6,24 @@
 #include <queue>
 #include <stack>
 #include <unordered_map>
+#include <climits>
 
-#define gcd(a, b) __gcd(a, b) // 最大公约数
+#define PI 3.14159265358979323846
+#define gcd(a, b) __gcd(a, b)             // 最大公约数
 #define bitcount(a) __builtin_popcount(a) // 二进制中1的个数
-#define lcm(a, b) (a * b / gcd(a, b)) // 最小公倍数
-#define max(a, b) (a > b ? a : b) // 最大值
-#define min(a, b) (a < b ? a : b) // 最小值
-#define abs(a) (a > 0 ? a : -a) // 绝对值
-#define swap(a, b) {int t = a; a = b; b = t;} // 交换
+#define lcm(a, b) (a * b / gcd(a, b))     // 最小公倍数
+#define max(a, b) (a > b ? a : b)         // 最大值
+#define min(a, b) (a < b ? a : b)         // 最小值
+#define abs(a) (a > 0 ? a : -a)           // 绝对值
+#define swap(a, b) \
+    {              \
+        int t = a; \
+        a = b;     \
+        b = t;     \
+    }                                             // 交换
 #define rep(i, a, b) for (int i = a; i <= b; i++) // 递增
 #define per(i, a, b) for (int i = a; i >= b; i--) // 递减
-#define forin(i, a) for (auto i : a) // 遍历
+#define forin(i, a) for (auto i : a)              // 遍历
 #define ll long long
 #define ull unsigned long long
 
@@ -34,53 +41,32 @@
 using namespace std;
 
 
-
-
-
-
-
-
-
-
-
 // ———————————————— Solution ————————————————
+
 
 class Solution {
 public:
-    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2) {
-        int m = nums1.size(), n = nums2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1)); // dp[i][j] 表示 nums1[0..i-1] 和 nums2[0..j-1] 的最长公共子序列
-        for (int i = 1; i <= m; i++) {
-            int num1 = nums1[i - 1];
-            for (int j = 1; j <= n; j++) {
-                int num2 = nums2[j - 1];
-                if (num1 == num2) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1; // 如果两个数相等，那么最长公共子序列就是两个数之前的最长公共子序列加1
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); // 两个序列都不包含 nums1[i-1] 和 nums2[j-1]
-                }
-            }
+    vector<vector<int>> intervalIntersection(vector<vector<int>> &firstList, vector<vector<int>> &secondList) {
+        if (firstList.empty() || secondList.empty()) return {};
+        vector<vector<int>> res;
+        int i = 0, j = 0; // 双指针
+        while (i < firstList.size() && j < secondList.size()) {
+            int left = max(firstList[i][0], secondList[j][0]); // 左边界
+            int right = min(firstList[i][1], secondList[j][1]); // 右边界
+            if (left <= right) res.push_back({left, right}); // 有交集
+            (firstList[i][1] < secondList[j][1] ? i : j)++;
         }
-        return dp[m][n];
+        return res;
     }
 };
 
 
-
-
-
-
-
 // ———————————————— Main ————————————————
+#include <stdio.h>
+#include <stdlib.h>
+
+int class1, class2, class3;
 
 int main() {
-    Solution solution;
-    int numa, numb;
-    cin >> numa >> numb;
-    solution.solution(numa, numb);
-    cout << solution.solution(numa, numb) << endl;
-    return 0;
-}
 
-
-
+};
