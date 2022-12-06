617dca6e772b2708958c297d58779012ab0a7eb0
w1z4Rd <yaojinqing@kayleh.top>
2022-12-06 13:18:34 +0800

    auto update~

--- src/RUN/java/Solution.java
+++ src/RUN/java/Solution.java
@@ -1,40 +1,27 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.HashSet;
 
 class Solution {
-    public int minScore(int n, int[][] roads) {
-        // 建图
-        List[] e = new ArrayList[n + 1];    // 邻接表
-        List[] v = new ArrayList[n + 1];    // 邻接表
-        for (int i = 1; i <= n; i++) {
-            e[i] = new ArrayList<Integer>();
-            v[i] = new ArrayList<Integer>();
-        }
-        for (int[] road : roads) {
-            e[road[0]].add(road[1]);
-            v[road[0]].add(road[2]);
-            e[road[1]].add(road[0]);
-            v[road[1]].add(road[2]);
-        }
+    int maxSplit = 0;
+    private HashSet<String> hashSet;
 
-        int ans = Integer.MAX_VALUE;
+    public int maxUniqueSplit(String s) {
+        hashSet = new HashSet<String>();
+        dfs(s, 0);
+        return maxSplit;
+    }
 
-        //bfs
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1);
-        boolean[] visited = new boolean[n + 1];
-        while (!queue.isEmpty()) {
-            Integer sn = queue.poll(); // 枚举sn的所有边
-            for (int i = 0; i < e[sn].size(); i++) {
-                ans = Math.min(ans, (int) v[sn].get(i));
-                int en = (int) e[sn].get(i);
-                if (visited[en]) continue;
-                visited[en] = true;
-                queue.offer(en);
+    private void dfs(String s, int i) {
+        if (i == s.length()) {
+            maxSplit = Math.max(maxSplit, hashSet.size());
+            return;
+        }
+        for (int j = i; j < s.length(); j++) {
+            String sub = s.substring(i, j + 1);
+            if (!hashSet.contains(sub)) {
+                hashSet.add(sub);
+                dfs(s, j + 1);
+                hashSet.remove(sub);
             }
         }
-        return ans;
     }
 }
