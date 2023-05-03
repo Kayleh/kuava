b25938a21eaf12104e4c473bffc9ffa4411e8698
zastava <yaojinqing@kayleh.top>
2023-05-04 00:34:18 +0800

    update

--- src/RUN/cpp/Solution.cpp
+++ src/RUN/cpp/Solution.cpp
@@ -32,22 +32,17 @@
 // #include <cassert>
 #include <unordered_set>
 
-#ifdef kOS
-#define INPUT_FILE "C:/input.txt"
-#define OUTPUT_FILE "C:/output.txt"
-#endif
-
 #define PI 3.14159265358979323846
-#define gcd(a, b) __gcd(a, b)                             // 最大公约数
-#define bitcount(a) __builtin_popcount(a)                 // 二进制中1的个数
-#define lcm(a, b) (a * b / gcd(a, b))                     // 最小公倍数
-#define max(a, b) (a > b ? a : b)                         // 最大值
-#define min(a, b) (a < b ? a : b)                         // 最小值
-#define abs(a) (a > 0 ? a : -a)                           // 绝对值
-#define swapp(a, b) (a ^= b; b ^= a; a ^= b)              // 交换
-#define forin(item, arr) for (auto item : arr)            // 遍历
-#define rep(i, from, to) for (int i = from; i <= to; i++) // 递增
-#define per(i, from, to) for (int i = from; i >= to; i--) // 递减
+#define gcd(a, b) __gcd(a, b)             // 最大公约数
+#define bitcount(a) __builtin_popcount(a) // 二进制中1的个数
+#define lcm(a, b) (a * b / gcd(a, b))     // 最小公倍数
+// #define max(a, b) (a > b ? a : b)                        // 最大值
+// #define min(a, b) (a < b ? a : b)                        // 最小值
+#define abs(a) (a > 0 ? a : -a)                          // 绝对值
+#define swapp(a, b) (a ^= b; b ^= a; a ^= b)             // 交换
+#define forin(item, arr) for (auto item : arr)           // 遍历
+#define rr(i, from, to) for (int i = from; i <= to; i++) // 递增
+#define dr(i, from, to) for (int i = from; i >= to; i--) // 递减
 #define ll long long
 #define ull unsigned long long
 #define changeCase(c) (c ^ (1 << 5))           // 大小写互换
@@ -55,45 +50,54 @@
 #define INF 0x3f3f3f3f                         // 无穷大
 #define NINF 0xc0c0c0c0                        // 无穷小
 #define randd(a, b) (rand() % (b - a + 1) + a) // [a, b]
-
-template<typename T>
+template <typename T>
 bool chkMax(T &x, T y) { return (y > x) ? x = y, 1 : 0; }
-
-template<typename T>
+template <typename T>
 bool chkMin(T &x, T y) { return (y < x) ? x = y, 1 : 0; }
-
-auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) { return a.second < b.second; };
-
-#ifdef debug
-struct ListNode
+/* struct ListNode
 {
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-#endif
+}; */
+/* struct TreeNode{int val;TreeNode *left;  TreeNode *right;    TreeNode() : val(0), left(nullptr), right(nullptr) {}    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}}; */
 
 using namespace std;
-// #define debug
 
 //@start——————————————————————————————————————————————————————————————————————
-
-const int N = 2010; // 最大点数
-
-class Solution {
+class Solution
+{
 public:
-    int findCheapestPrice(int n, vector <vector<int>> &flights, int src, int dst, int k) {
+    int maxCompatibilitySum(vector<vector<int>> &students, vector<vector<int>> &mentors)
+    {
+        int m = students.size();                  // 学生数
+        int n = students[0].size();               // 题目数
+        vector<vector<int>> g(m, vector<int>(m)); // g[i][j] 表示学生i和导师j的匹配度
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                for (int k = 0; k < n; ++k)
+                {
+                    g[i][j] += (students[i][k] == mentors[j][k]);
+                }
+            }
+        }
 
+        vector<int> p(m);
+        iota(p.begin(), p.end(), 0);
+        int ans = 0;
+        do
+        {
+            int cur = 0;
+            for (int i = 0; i < m; ++i)
+            {
+                cur += g[i][p[i]];
+            }
+            ans = max(ans, cur);
+        } while (next_permutation(p.begin(), p.end()));
+        return ans;
     }
 };
