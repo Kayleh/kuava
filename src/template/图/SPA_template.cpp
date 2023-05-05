5b8fa70578e4f62f8c4541c86e96e85d15b87a4b
zastava <yaojinqing@kayleh.top>
2023-05-05 23:24:14 +0800

    update

--- src/template/图/SPA_template.cpp
+++ src/template/图/SPA_template.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Q: 给你一个带正权的无向连通图，求最多经过k个点的最短路
+// Q: 给你一个带正权的无向连通图，求最多经过k个点的最短路的权值和
+// 1。bellman-ford
 int bellmanFord(int n, vector<vector<int>> &flights, int src, int dst, int k)
 {
     const int INF = 0x3f3f3f3f;
@@ -19,4 +20,39 @@ int bellmanFord(int n, vector<vector<int>> &flights, int src, int dst, int k)
         dis = temp;
     }
     return dis[dst] == INF ? -1 : dis[dst];
+}
+
+// 2.dfs
+int dfs(int n, vector<vector<int>> &flights, int src, int dst, int k)
+{
+    const int INF = 0x3f3f3f3f;
+    vector<vector<int>> g(n, vector<int>(n, INF)); // g[i][j] 表示i到j的距离
+    for (auto &flight : flights)
+    {
+        int a = flight[0], b = flight[1], c = flight[2];
+        g[a][b] = c;
+    }
+    vector<int> vis(n, 0); // vis[i] 表示i是否被访问过
+    int ans = INF;
+    function<void(int, int, int)> dfs = [&](int u, int d, int cost)
+    {
+        if (d > k + 1)
+            return;
+        if (u == dst)
+        {
+            ans = min(ans, cost);
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (vis[i] || g[u][i] == INF)
+                continue;
+            vis[i] = 1;
+            dfs(i, d + 1, cost + g[u][i]);
+            vis[i] = 0;
+        }
+    };
+    vis[src] = 1;
+    dfs(src, 0, 0);
+    return ans == INF ? -1 : ans;
 }
