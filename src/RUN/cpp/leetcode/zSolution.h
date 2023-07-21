1b03bde02db6a32c6fd5aca7378293bcd3c9f424
zastava <git@kayleh.top>
2023-07-22 00:09:50 +0800

    背包~

--- src/RUN/cpp/leetcode/zSolution.h
+++ src/RUN/cpp/leetcode/zSolution.h
@@ -93,26 +93,26 @@ using namespace std;
 class Solution
 {
 public:
-    ListNode *detectCycle(ListNode *head)
+    int change(int amount, vector<int> &coins)
     {
-        ListNode *slow = head, *fast = head;
-        while (fast != nullptr)
+        int n = coins.size();
+        int dp[n + 1][amount + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i <= n; i++)
         {
-            slow = slow->next;
-            if (fast->next == nullptr)
-                return nullptr;
-            fast = fast->next->next;
-            if (fast == slow) // 有环, 且此时fast和slow相遇
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= amount; j++)
             {
-                ListNode *ptr = head; // ptr从头开始走, slow继续走, 相遇时即为环的入口
-                while (ptr != slow)
+                dp[i][j] = dp[i - 1][j];
+                if (j - coins[i - 1] >= 0)
                 {
-                    ptr = ptr->next;
-                    slow = slow->next;
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
                 }
-                return ptr;
             }
         }
-        return nullptr;
+        return dp[n][amount];
     }
 };
